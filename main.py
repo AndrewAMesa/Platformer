e8e40a89677dc55245e8a3101998544d2d2b4f7e
Andy <aamesa03@gmail.com>
2021-03-19 10:09:12 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+
+class main():
+    DISPLAYWIDTH = 15
+    DISPLAYHEIGHT = 15
+    TILESIZE = 30
+    DISPLAYSURF = pygame.display.set_mode((DISPLAYWIDTH * TILESIZE, DISPLAYHEIGHT * TILESIZE))
+    def main(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+MainObject = main()
+MainObject.main()
