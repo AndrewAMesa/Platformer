9f2554a0ee86fff8698261bfa07ed6abbb34ab12
drog414 <dennyrogers4@gmail.com>
2021-03-30 12:41:15 -0600

    Interpreter

--- main.py
+++ main.py
@@ -22,37 +22,5 @@ class main():
             #Update the Screen
             pygame.display.update()
 
-    def parseText(self):
-        self.count = 0
-        file = open(self.level)
-        line = file.readline()
-        tempX = 0
-        for x in range(0, len(line)):
-            if line[x] == "i":
-                self.startingSpotX = tempX
-                self.startingSpotY = self.count
-                self.BOARDWIDTH += 1
-                tempX += 1
-                self.lineList.append(line[x])
-            elif line[x] != ',' and line[x] != '\n':
-                self.BOARDWIDTH += 1
-                tempX += 1
-                self.lineList.append(line[x])
-        self.count += 1
-        while line:
-            line = file.readline()
-            tempX = 0
-            for x in range(0, len(line)):
-                if line[x] == "i":
-                    self.startingSpotX = tempX
-                    self.startingSpotY = self.count
-                    tempX += 1
-                    self.lineList.append(line[x])
-                elif line[x] != "," and line[x] != "\n":
-                    tempX += 1
-                    self.lineList.append(line[x])
-            self.count += 1
-        self.count -= 1
-
 MainObject = main()
 MainObject.main()
