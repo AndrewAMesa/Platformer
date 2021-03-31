744fed8d7c2a8e924b613dcd11a3a23b33cf58f3
Andy <aamesa03@gmail.com>
2021-03-31 13:07:10 -0600

    2nd change

--- sprites.py
+++ sprites.py
@@ -1,6 +1,19 @@
+import pygame, sys
+from pygame.locals import *
+
 class MainCharacter:
-
-
+    print("hi")
 class Platform:
+    print("hi")
 
 class Sword:
+    def __init__(self, _left, _top, _image):
+        pygame.sprite.Sprite.__init__(self)
+        self.left = _left
+        self.top = _top
+        self.image = _image
+        self.rect = self.image.get_rect()
+        self.rect.update(self.left, self.top, self.rect.width, self.rect.height)
+        self.count = 0
+        self.xMove = 0
+        self.yMove =0
