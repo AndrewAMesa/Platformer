e4e59d7a6a27f8cd1fd84ce4c7d5a9db57edc706
drog414 <dennyrogers4@gmail.com>
2021-04-01 13:01:48 -0600

    Basic Block classes outline

--- sprites.py
+++ sprites.py
@@ -1,5 +1,74 @@
+import pygame
+
 class MainCharacter:
 
+    #Basic initialization class that supports different directions for the sprite
+    def __init__(self, sprites, posX, posY, direction):
 
-class Platform:
+        super().__init__()
+        self.sprites = sprites
+        self.currentSprite = 0
+        self.image = self.sprites[self.currentSprite]
 
+        if direction != 0:
+            # For when the sprite is reversed
+            self.sprites1 = []
+            for i in range(len(self.sprites)):
+                self.sprites1.append(pygame.transform.flip(self.sprites[i], True, False))
+
+            self.direction = direction
+
+
+class Platform(pygame.sprite.Sprite):
+
+    def __init__(self, sprites, posX, posY, breakable, damage):
+
+        super().__init__()
+        self.sprites = sprites
+        self.currentSprite = 0
+        self.image = self.sprites[self.currentSprite]
+
+        #position values
+        self.rect = self.image.get_rect()
+        self.posX = posX
+        self.posY = posY
+        self.rect.topleft = [posX, posY]
+
+        #Object information
+        self.breakable = breakable           #If True, destroy block in response to damage
+        self.damage = damage                 #For Blocks such as spikes and lava, amount of damage inflicted to the player
+
+class BasicBlock(Platform):
+
+    def __init__(self, sprites, posX, posY):
+
+        #Load Images
+
+        super.__init__(sprites, posX, posY, False, 0)
+
+
+class BreakableBlock(Platform):
+
+    def __init__(self, sprites, posX, posY):
+
+        # Load Images
+
+        super.__init__(sprites, posX, posY, True, 0)
+
+class SpikesBlock(Platform):
+
+    def __init__(self, sprites, posX, posY):
+
+        # Load Images
+
+        super.__init__(sprites, posX, posY, False, 5)
+
+class LavaBlock(Platform):
+
+    def __init__(self, sprites, posX, posY):
+
+        # Load Images
+        self.sprites = []
+        self.sprites.append(pygame.image.load('Images/Lava.png'))
+
+        super.__init__(sprites, posX, posY, False, 5)
