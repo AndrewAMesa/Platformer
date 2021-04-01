c45d1d369ff462c72e2c9cd98a381bd760fa6144
Brian Daley <22brian.daley@boiseschools.net>
2021-04-01 12:58:58 -0600

    Trying to help Brian

--- sprites.py
+++ sprites.py
@@ -1,5 +1,19 @@
-class MainCharacter:
+import pygame
 
 
-class Platform:
+class MainCharacter(pygame.sprite.Sprite):
+    def __init__(self, DISPLAYSURF):
+        super().__init__()
+        self.image = pygame.Surface((20, 20))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.center = (DISPLAYSURF.get_width() / 2, DISPLAYSURF.get_height() / 2)
 
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, DISPLAYSURF):
+        super().__init__()
+        self.image = pygame.Surface((100, 20))
+        self.image.fill((0, 0, 255))
+        self.rect = self.image.get_rect()
+        self.rect.center = (DISPLAYSURF.get_width() / 2, DISPLAYSURF.get_height() / 2 + self.image.get_height())
