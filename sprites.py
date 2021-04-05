de6326732273a4e6707cd29796e81459d5994bc8
ReaganWal <21reagan.walhof@boiseschools.net>
2021-04-05 12:43:16 -0600

    adding

--- sprites.py
+++ sprites.py
@@ -1,6 +1,34 @@
-class MainCharacter:
+import pygame, sys
+pygame.init()
+#class MainCharacter:
 
 
-class Platform:
+#class Platform:
 
-class Collectables:
+class Collectables(pygame.sprite.Sprite):
+    def __init__(self, string, xpos, ypos, image):
+        super().__init__()
+        self.name = string
+        self.xpos = xpos
+        self.ypos = ypos
+        self.rect.update(xpos, ypos, 100, 100)
+        self.image=pygame.image.load(image)
+
+    def is_collided_with(self, char):
+
+        if self.rect.colliderect(char.rect):
+
+            self.kill()
+
+    def getname(self):
+        return self.name
+
+    def updatepos(self, x, y):
+        self.posx = x
+        self.posy = y
+
+    def getposx(self):
+        return self.posx
+
+    def getposy(self):
+        return self.posy
