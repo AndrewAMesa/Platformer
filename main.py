0f84640485fdb088dc9776f87b81cc050ce938f2
Andy <aamesa03@gmail.com>
2021-04-20 12:57:01 -0600

    "first"

--- main.py
+++ main.py
@@ -73,10 +73,21 @@ def update_all():
     character_group.update(sword, gun)
     shiftX, shiftY = main_character.getShift()
     enemyMovement()
-    platform_group.update(shiftX, shiftY)
+    for enemy in enemy_group:
+        if enemy.velocityX != 0:
+            for platform in platform_group:
+                if enemy.rect.bottom > platform.rect.top and enemy.rect.top < platform.rect.bottom:
+                    if enemy.currentDirection > 0:
+                        if enemy.rect.right + (
+                                enemy.currentDirection * enemy.velocityX) >= platform.rect.left >= enemy.rect.right and not platform.walkthrough:
+                            enemy.currentDirection *= -1
+                    if enemy.currentDirection < 0:
+                        if enemy.rect.left + (
+                                enemy.currentDirection * enemy.velocityX) <= platform.rect.right <= enemy.rect.left and not platform.walkthrough:
+                            enemy.currentDirection *= -1
     enemy_group.update(shiftX, shiftY)
-    # for collectable in collectable_group:
-    #    collectable.is_collided_with(main_character)
+    platform_group.update(shiftX, shiftY)
+
 
 
 def checkcollision(char, group):
@@ -170,8 +181,8 @@ def main():
         character_group.draw(DISPLAYSURF)
         current_weapon.draw(DISPLAYSURF)
         bullet_group.draw(DISPLAYSURF)
-        platform_group.draw(DISPLAYSURF)
         enemy_group.draw(DISPLAYSURF)
+        platform_group.draw(DISPLAYSURF)
         main_character.displayhealth(DISPLAYSURF)
         display_time(milliseconds)
         gunMilliseconds = update_gun(gunMilliseconds)
